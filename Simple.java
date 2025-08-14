e745b03d39ea50f0517d2cda20087905e3528a77
KumarChinthala10 <kumarmrce@gmail.com>
2025-08-14 23:54:57 +0530

    Add files via upload
    Basic java program

---
+++ Simple.java
@@ -0,0 +1,21 @@
+public class Simple {
+
+    public static void main(String[] args) {
+        int age=34;
+        double salary =54000.75;
+        char grade ='A';
+        boolean isJavaFun = true;
+        string name ='Kumar';
+
+        System.out.println("Name: "+ Name);
+        System.out.println("Age: "+ age);
+        System.out.println("Salary: "+ salary);
+        System.out.println("Grage: "+ grade);
+        System.out.println("IsJavaFun: "+ IsJavaFun);
+
+    }
+}
+
+
+
+
